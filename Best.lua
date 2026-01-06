f60bfb31079f23a99ebb073cc51643cbfb4221f3
Tweeksscript <tweeksscripts@proton.me>
2026-01-06 19:07:16 +0100

    Update and rename GPTxYield.lua to Best.lua

--- Best.lua
+++ Best.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 
 -- EDUCATIONAL PURPOSES (UI / math / camera techniques)
 
